cbcbb5675d47b0039ba0ce45172b9653a4899213
Evelina Enqvist <evelina.enqvist@brightstraining.com>
2024-10-24 15:49:53 +0200

    Said hellos

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
 ﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
 
+Console.WriteLine("Hej hej hej här är jag :)");
 Console.WriteLine("Hello, World!");
-Console.WriteLine("I live in Sweden");
+<<<<<<< HEAD
+Console.WriteLine("I live in Denmark");
 Console.WriteLine("I like pandas");
+=======
+Console.WriteLine("I live in Sweden!");
+Console.WriteLine("I like pandas!");
+>>>>>>> exclamation
 
 string text = System.IO.File.ReadAllText("panda-info.txt");
-System.Console.WriteLine($"Here in info from the file 👉 {text}");
+System.Console.WriteLine($"Here in info from the file 👉 {text}!");
+
 
 
